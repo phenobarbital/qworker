2a87412f119c8cda86e2632b592bec9850dedbc7
Jesus Lara <jesuslarag@gmail.com>
2023-06-06 18:55:51 +0200

    new fixes on QW

--- qw/executor/__init__.py
+++ qw/executor/__init__.py
@@ -1,9 +1,16 @@
 import asyncio
 import inspect
 from concurrent.futures import ThreadPoolExecutor
+import multiprocessing as mp
 from navconfig.logging import logging
+from notify.providers.telegram import Telegram
+from notify.models import Chat
+from flowtask.conf import (
+    EVENT_CHAT_ID,
+    ENVIRONMENT
+)
 from ..wrappers import QueueWrapper, FuncWrapper, TaskWrapper
-from ..conf import WORKER_CONCURRENCY_NUMBER
+from ..conf import WORKER_CONCURRENCY_NUMBER, WORKER_TASK_TIMEOUT
 
 class TaskExecutor:
     def __init__(self, task, *args, **kwargs):
@@ -18,12 +25,65 @@ class TaskExecutor:
         )
         try:
             await self.task.create()
-            result = await self.task.run()
+            task = asyncio.create_task(self.task.run())
+            task.add_done_callback(self.task_done)
+            _, pending = await asyncio.wait(
+                [task], timeout=WORKER_TASK_TIMEOUT * 60
+            )
+            if task in pending:
+                await self.task_pending(task)
+                task.cancel()
+                raise asyncio.TimeoutError(
+                    f"Task {self.task} was cancelled."
+                )
+            # get the result:
+            result = task.result()
         except Exception as err:  # pylint: disable=W0703
+            self.logger.error(
+                f"An Error occurred while running Task {self.task}"
+            )
             result = err
         finally:
             await self.task.close()
             return result
+
+    def task_done(self, task, *args, **kwargs):
+        self.logger.notice(
+            f"Finalized Task {task}"
+        )
+
+    def get_notify(self):
+        # TODO: implement other notify connectors:
+        # defining the Default chat object:
+        recipient = Chat(**{"chat_id": EVENT_CHAT_ID, "chat_name": "Navigator"})
+        # send notifications to Telegram bot
+        tm = Telegram()
+        return [tm, recipient]
+
+    async def task_pending(self, task, *args, **kwargs):
+        worker = mp.current_process()
+        message = f"Task {self.task!s} got timeout and was cancelled at {worker.name!s}"
+        self.logger.error(
+            message
+        )
+        try:
+            ntf, recipients = self.get_notify()
+            message = f'⚠️ ::{ENVIRONMENT} -  {message!s}.'
+            args = {
+                "recipient": [recipients],
+                "message": message,
+                "disable_notification": True
+            }
+            try:
+                async with ntf as conn:
+                    result = await conn.send(**args)
+                return result
+            except RuntimeError:
+                pass
+        except Exception as err:
+            self.logger.error(
+                f"Error Sending Task Failure notification: {err}"
+            )
 
     async def run(self):
         result = None
@@ -32,18 +92,30 @@ class TaskExecutor:
         except RuntimeError:
             loop = asyncio.get_event_loop()
         try:
-            if isinstance(self.task, (FuncWrapper, QueueWrapper)):
+            if type(self.task) in (FuncWrapper, QueueWrapper):
+                self.logger.notice(
+                    f"Running Function: {self.task}"
+                )
                 self.task.set_loop(loop)
                 result = await self.task()
             elif isinstance(self.task, TaskWrapper):
+                self.logger.notice(
+                    f"Running Task: {self.task}"
+                )
                 self.task.set_loop(loop)
                 async with self.semaphore:
                     result = await self.run_task()
             elif (
                 inspect.isawaitable(self.task) or asyncio.iscoroutinefunction(self.task)
             ):
+                self.logger.notice(
+                    f"Running Awaitable Func: {self.task}"
+                )
                 result = await self.task()
             else:
+                self.logger.notice(
+                    f"Running Blocking Function: {self.task}"
+                )
                 with ThreadPoolExecutor(max_workers=2) as executor:
                     future = loop.run_in_executor(executor, self.task)
                 result = await future
