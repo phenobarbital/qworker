57d6725dafda57f6ac8f7f48c53bbd1d4133adf4
Jesus Lara <jesuslarag@gmail.com>
2026-04-08 18:04:16 +0200

    using old code on health.py

--- qw/version.py
+++ qw/version.py
@@ -1,14 +1,14 @@
 """Queue Worker Meta information.
-   QueueWorker is a asyncio-based Worker for distributed functions.
+    QueueWorker is a asyncio-based Worker for distributed functions.
 """
 
 __title__ = 'qworker'
 __description__ = (
-   'QueueWorker is asynchronous Task Queue implementation '
-   'built on top of Asyncio.'
-   'Can you spawn distributed workers to run functions inside workers.'
+    'QueueWorker is asynchronous Task Queue implementation '
+    'built on top of Asyncio.'
+    'Can you spawn distributed workers to run functions inside workers.'
 )
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Jesus Lara'
 __author_email__ = 'jesuslarag@gmail.com'
 __license__ = 'MIT'
@@ -16,4 +16,4 @@ __license__ = 'MIT'
 def get_version() -> tuple:  # pragma: no cover
     """ Get Queue Worker version as tuple.
     """
-    return tuple(x for x in __version__.split('.'))  # pragma: no cover
+    return tuple(__version__.split('.'))  # pragma: no cover
